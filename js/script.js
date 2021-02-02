5603c2ea2dce29135be00731337034031c4a43da
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-02-02 10:33:07 +0100

    dinamic contact

---
+++ js/script.js
@@ -0,0 +1,49 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    user:{
+      name: "Jane Doe",
+      img: "img/avatar_io.jpg"
+    },
+    contacts: [
+      {
+        name: "Michele",
+        img: "img/avatar_1.jpg",
+        current_chat: true
+      },
+      {
+        name: "Fabio",
+        img: "img/avatar_2.jpg",
+        current_chat: false
+      },
+      {
+        name: "Samuele",
+        img: "img/avatar_3.jpg",
+        current_chat: false
+      },
+      {
+        name: "Giorgio",
+        img: "img/avatar_4.jpg",
+        current_chat: false
+      }
+    ]
+  },
+  methods: {
+    // cambiare utente in chatbox
+    changeChat(index) {
+      if(this.contacts[index].current_chat != true) {
+
+        const contact = this.contacts.splice(index, 1)[0];
+        contact.current_chat = true
+
+        this.contacts.forEach((element) => {
+          if(element.current_chat == true) {
+            element.current_chat = false;
+          }
+        });
+
+        this.contacts.splice(index, 0, contact);
+      }
+    }
+  }
+});
